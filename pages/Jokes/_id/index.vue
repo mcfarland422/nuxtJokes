8dbc0f2fbff1f5d3be85cae910b2fb1e7261911f
Michael McFarland <gardengrove422@gmail.com>
2020-08-12 18:14:03 -0400

    first

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>{{ $route.params.id }}</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      joke: {},
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+
+      // console.log(res.data);
+      this.joke = res.data.joke;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
+</script>
+
+<style>
+</style>
